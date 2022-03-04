6e15aad2517b3518fc4d0604c1916e4bc0889ad6
Pascal Van Cauwenberghe <pvc_git@nayima.be>
2022-03-04 08:28:40 +0100

    Log retries

---
+++ src/main/java/be/nayima/blueprint/async/generic/processor/MessageHeader.java
@@ -0,0 +1,7 @@
+package be.nayima.blueprint.async.generic.processor;
+
+public class MessageHeader {
+    public static final String DELIVERY_ATTEMPT = "deliveryAttempt";
+    public static final String X_DEATH = "x-death";
+    public static final String X_DEATH_COUNT = "count";
+}
--- src/main/java/be/nayima/blueprint/async/persistent/processor/PersistentJobProcessor.java
+++ src/main/java/be/nayima/blueprint/async/persistent/processor/PersistentJobProcessor.java
@@ -1,5 +1,6 @@
 package be.nayima.blueprint.async.persistent.processor;
 
+import be.nayima.blueprint.async.generic.processor.MessageHeader;
 import be.nayima.blueprint.async.persistent.connector.CallFailedException;
 import be.nayima.blueprint.async.persistent.message.PersistentJob;
 import be.nayima.blueprint.async.persistent.usecase.FailingPartyCaller;
@@ -19,9 +20,6 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-// Generic processor which is fed from a queue of DroppableJob<Job>
-// The processor uses the DroppableJob envelope to determine if the job has exceeded its TTL in the queue
-// The IPerformDroppableWork<Job> implementation handles both "fresh" (perform) and "stale" (drop) jobs.
 public class PersistentJobProcessor {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.from(ZoneOffset.UTC));
     private final FailingPartyCaller usecase;
@@ -41,8 +39,12 @@ public class PersistentJobProcessor {
             lastReceived = in.getCounter();
         } catch (CallFailedException e) {
             var headers = msg.getHeaders();
+/*            for (var headername : headers.keySet()) {
+                log.info("Header {}='{}' = {}", headername, headers.get(headername), headers.get(headername).getClass().getName());
+            }*/
+            var deliveryAttempts = attemptsIn(headers);
             var resurrections = resurrections(headers);
-            log.info("FAIL. Message {} processing at {}. {}. Resurrected {} times", in.getCounter(), Instant.now(), ordered(in), resurrections);
+            log.info("FAIL. Message {} processing at {} on attempt {}. {}. Resurrected {} times", in.getCounter(), Instant.now(), deliveryAttempts, ordered(in), resurrections);
 
             if (resurrections > MAX_RESURRECTIONS) {
                 log.error("Maximum resurrections reached for message #{}. Leaving it in peace", in.getCounter());
@@ -53,17 +55,30 @@ public class PersistentJobProcessor {
 
     }
 
+    private int attemptsIn(MessageHeaders headers) {
+        var value = headers.get(MessageHeader.DELIVERY_ATTEMPT);
+        if (value != null) {
+            try {
+                var nb = (Number) value;
+                return nb.intValue();
+            } catch (Exception e) {
+                log.warn("Header {} doesn't contain a number", MessageHeader.DELIVERY_ATTEMPT);
+            }
+        }
+        return 0;
+    }
+
     private String ordered(PersistentJob in) {
         return in.getCounter() < lastReceived ? "Out of order" : "In order";
     }
 
     private int resurrections(MessageHeaders headers) {
-        Object xDeath = headers.get("x-death");
+        Object xDeath = headers.get(MessageHeader.X_DEATH);
         if (xDeath != null) {
             var deathList = (List) xDeath;
             if (deathList.size() > 0) {
                 var deathMap = (Map) deathList.get(0);
-                var count = deathMap.get("count");
+                var count = deathMap.get(MessageHeader.X_DEATH_COUNT);
                 var countL = (Long) count;
                 return countL.intValue();
             }
